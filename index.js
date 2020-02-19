274b74261b67be782183ec77d1521038bf6943a2
LionelBrouard <lionel.brouard@gmail.com>
2020-02-19 20:46:07 +0100

    ajout de la route get/perso

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const formidableMiddleware = require("express-formidable");
 const cors = require("cors");
+const axios = require("axios");
+const md5 = require("md5");
+// const router = express.Router();
 
 // mongoose.connect(process.env.MONGODB_URI, {
 //   useNewUrlParser: true,
@@ -12,8 +15,29 @@ const cors = require("cors");
 const app = express();
 app.use(formidableMiddleware());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hi" });
+app.get("/perso", async (req, res) => {
+  const publickey = process.env.PUBLIC_KEY;
+  const privatekey = process.env.SECRET_KEY;
+  const date = new Date();
+  const ts = date.getTime().toString();
+  const stringToHash = ts + privatekey + publickey;
+  const hash = md5(stringToHash);
+  console.log(ts);
+  console.log(publickey);
+  console.log(hash);
+  try {
+    const response = await axios.get(
+      "http://gateway.marvel.com/v1/public/characters?ts=" +
+        ts +
+        "&apikey=" +
+        publickey +
+        "&hash=" +
+        hash
+    );
+    res.json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 app.all("*", function(req, res) {
@@ -23,3 +47,5 @@ app.all("*", function(req, res) {
 app.listen(3000, () => {
   console.log("Server has started");
 });
+
+// module.exports = router;
