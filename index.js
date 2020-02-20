32978cc2219d054fbf92896b59e4bed8dfd8ce2d
LionelBrouard <lionel.brouard@gmail.com>
2020-02-20 16:01:12 +0100

    Ajout de la route comics

--- index.js
+++ index.js
@@ -14,8 +14,62 @@ const md5 = require("md5");
 
 const app = express();
 app.use(formidableMiddleware());
+app.use(cors());
 
 app.get("/perso", async (req, res) => {
+  const publickey = process.env.PUBLIC_KEY;
+  const privatekey = process.env.SECRET_KEY;
+  const date = new Date();
+  const ts = date.getTime().toString();
+  const stringToHash = ts + privatekey + publickey;
+  const hash = md5(stringToHash);
+
+  try {
+    const response = await axios.get(
+      "http://gateway.marvel.com/v1/public/characters?ts=" +
+        ts +
+        "&apikey=" +
+        publickey +
+        "&hash=" +
+        hash +
+        "&limit=100"
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/unique_perso/:id", async (req, res) => {
+  const publickey = process.env.PUBLIC_KEY;
+  const privatekey = process.env.SECRET_KEY;
+  const date = new Date();
+  const ts = date.getTime().toString();
+  const stringToHash = ts + privatekey + publickey;
+  const hash = md5(stringToHash);
+
+  try {
+    const response = await axios.get(
+      "http://gateway.marvel.com/v1/public/characters/" +
+        req.params.id +
+        "/comics?ts=" +
+        ts +
+        "&apikey=" +
+        publickey +
+        "&hash=" +
+        hash
+    );
+    console.log("response.data    ", response.data);
+
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/comics", async (req, res) => {
   const publickey = process.env.PUBLIC_KEY;
   const privatekey = process.env.SECRET_KEY;
   const date = new Date();
@@ -27,15 +81,17 @@ app.get("/perso", async (req, res) => {
   console.log(hash);
   try {
     const response = await axios.get(
-      "http://gateway.marvel.com/v1/public/characters?ts=" +
+      "http://gateway.marvel.com/v1/public/comics?ts=" +
         ts +
         "&apikey=" +
         publickey +
         "&hash=" +
-        hash
+        hash +
+        "&limit=100"
     );
     res.json(response.data);
   } catch (error) {
+    console.log(error.message);
     return res.status(500).json({ message: error.message });
   }
 });
